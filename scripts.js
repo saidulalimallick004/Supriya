6603b2b0b31267cb728cccf2f7c85db7ebf7733c
saidulalimallick004 <saidulalimallick1@gmail.com>
2025-02-16 20:57:56 +0530

    Version 1.0.3

--- scripts.js
+++ scripts.js
@@ -40,9 +40,15 @@ let letter = '';
     letter = currentText.slice(0, ++index);
 
     document.querySelector('.typewriter').textContent = letter;
+
     if (letter.length === currentText.length) {
-        count++;
-        index = 0;
+        // Wait for 3 seconds before moving to the next text
+        setTimeout(() => {
+            count++;
+            index = 0;
+            type();
+        }, 3000);
+    } else {
+        setTimeout(type, 200); // Normal typing speed
     }
-    setTimeout(type, 200);
 })();
